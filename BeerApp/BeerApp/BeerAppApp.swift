1afb42f92e7f4af0a24d2245d700330bd3a863c6
Óscar Khergo <145770348+okhergo@users.noreply.github.com>
2023-12-01 17:05:33 +0100

    Add files via upload

---
+++ BeerApp/BeerApp/BeerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerAppApp.swift
+//  BeerApp
+//
+//  Created by Óscar Khergo on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BeerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
